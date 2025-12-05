b77398d8e4a4494cd0241968f23ff03cdefb4e7a
cnsqo <cnsqo0815@gmail.com>
2025-12-05 13:32:45 +0900

    add
    add

--- 3.java/work/layer/src/main/java/com/korea/layer/dto/ResponseDTO.java
+++ 3.java/work/layer/src/main/java/com/korea/layer/dto/ResponseDTO.java
@@ -1,22 +1,30 @@
 package com.korea.layer.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-// 클라이언트에게 데이터와 에러 메시지를 보내기 위한 DTO
-@Data
+//클라이언트에게 데이터와 에러 메시지를 보내기 위한 DTO
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class ResponseDTO<T> {
 
-    private String error;
-    
-    // TodoDTO 뿐만 아니라 이후에 다른 모델의 DTO도 ResponseDTO를 이용해 반환할 수 있도록 제네릭 타입을 사용.
-    private List<T> data;
-    
+	private String error;
+	//TodoDTO 뿐만 아니라 이후에 다른 모델의 DTO도 ResponseDTO를 이용해 반환할 수 있도록
+	//제네릭 타입을 사용했다.
+	private List<T> data;
 }
+
+
+
+
+
+
+
+
+
--- 3.java/work/layer/src/main/java/com/korea/layer/dto/TestDTO.java
+++ 3.java/work/layer/src/main/java/com/korea/layer/dto/TestDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class TestDTO {
-	
+
 	private int id;
 	private String message;
 }
